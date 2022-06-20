ef0b458eafe4d3dd00ed4ffd4211b98e2d4f0ba8
mark@archix <markchristianlacap@gmail.com>
2022-06-20 22:52:20 +0800

    fixed script

--- synch.sh
+++ synch.sh
@@ -1,9 +1,9 @@
 echo "synching config files...."
-cp ~/.config/polybar ./polybar -r
-cp ~/.config/sxhkd ./sxhkd -r
-cp ~/.config/rofi ./rofi -r
+cp ~/.config/polybar/ ./polybar -r
+cp ~/.config/sxhkd/ ./sxhkd -r
+cp ~/.config/rofi/ ./rofi -r
 cp ~/.config/picom.conf ./picom.conf -r
-cp ~/.config/bspwm ./bspm -r
-cp ~/.config/alacritty ./alacrity -r
+cp ~/.config/bspwm ./bspwm -r
+cp ~/.config/alacritty ./alacritty -r
 cp ~/.xinitrc .xinitrc -r
 echo "successfully synched"
