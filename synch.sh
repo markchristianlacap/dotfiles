f898d5b0398a7026ce9842529791c3eab372d5aa
mark <markchristianlacap@gmail.com>
2022-06-21 09:35:44 +0800

    update config

--- synch.sh
+++ synch.sh
@@ -3,7 +3,7 @@ cp ~/.config/polybar ./config/ -r
 cp ~/.config/sxhkd ./config/ -r
 cp ~/.config/rofi ./config/ -r
 cp ~/.config/bspwm ./config/ -r
-cp ~/.config/alacritty ./config/ -r
+cp ~/.config/kitty ./config/ -r
 cp ~/.config/picom.conf ./config/picom.conf -r
 cp ~/.xinitrc .xinitrc -r
 echo "successfully synched"
