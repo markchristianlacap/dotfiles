2870f5ecbe03cbfdc20833c6045a1c0055c0c450
mark <markchristianlacap@gmail.com>
2025-07-30 22:45:36 +0800

    feat: update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require("wezterm")
-local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 local config = wezterm.config_builder()
 config.font = wezterm.font_with_fallback({
-  "Iosevka Nerd Font Mono",
-  "JetBrains Nerd Font Mono",
+	"Iosevka Nerd Font Mono",
+	"JetBrains Nerd Font Mono",
 })
 config.font_size = 15
 config.color_scheme = "Kanagawa (Gogh)"
@@ -11,24 +11,26 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.9
 config.detect_password_input = false
 smart_splits.apply_to_config(config, {
-  -- the default config is here, if you'd like to use the default keys,
-  -- you can omit this configuration table parameter and just use
-  -- smart_splits.apply_to_config(config)
-
-  -- directional keys to use in order of: left, down, up, right
-  direction_keys = { 'h', 'j', 'k', 'l' },
-  -- if you want to use separate direction keys for move vs. resize, you
-  -- can also do this:
-  direction_keys = {
-    move = { 'h', 'j', 'k', 'l' },
-    resize = { 'LeftArrow', 'DownArrow', 'UpArrow', 'RightArrow' },
-  },
-  -- modifier keys to combine with direction_keys
-  modifiers = {
-    move = 'CTRL',   -- modifier to use for pane movement, e.g. CTRL+h to move left
-    resize = 'META', -- modifier to use for pane resize, e.g. META+h to resize to the left
-  },
-  -- log level to use: info, warn, error
-  log_level = 'info',
+	direction_keys = {
+		move = { "h", "j", "k", "l" },
+		resize = { "LeftArrow", "DownArrow", "UpArrow", "RightArrow" },
+	},
+	modifiers = {
+		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
+		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
+	},
+	log_level = "info",
 })
+config.keys = {
+	{ key = "W",     mods = "SHIFT|CTRL",     action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+	{ key = 'H',     mods = 'SHIFT|CTRL',     action = wezterm.action.ActivatePaneDirection 'Left' },
+	{ key = 'H',     mods = 'SHIFT|ALT|CTRL', action = wezterm.action.AdjustPaneSize { 'Left', 1 } },
+	{ key = 'L',     mods = 'SHIFT|CTRL',     action = wezterm.action.ActivatePaneDirection 'Right' },
+	{ key = 'L',     mods = 'SHIFT|ALT|CTRL', action = wezterm.action.AdjustPaneSize { 'Right', 1 } },
+	{ key = 'K',     mods = 'SHIFT|CTRL',     action = wezterm.action.ActivatePaneDirection 'Up' },
+	{ key = 'K',     mods = 'SHIFT|ALT|CTRL', action = wezterm.action.AdjustPaneSize { 'Up', 1 } },
+	{ key = 'J',     mods = 'SHIFT|CTRL',     action = wezterm.action.ActivatePaneDirection 'Down' },
+	{ key = 'J',     mods = 'SHIFT|ALT|CTRL', action = wezterm.action.AdjustPaneSize { 'Down', 1 } },
+	{ key = 'Enter', mods = 'ALT',            action = wezterm.action.DisableDefaultAssignment },
+}
 return config
