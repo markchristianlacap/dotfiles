73b6f6717539d64d0203d127adc3047d0f664731
Mark Christian Lacap <markchristianlacap@gmail.com>
2025-10-03 15:47:52 +0800

    feat: dunst config

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 cp $HOME/.config/picom .config/ -r
 cp $HOME/.config/sxhkd .config/ -r
 cp $HOME/.config/pipewire .config/ -r
+cp $HOME/.config/dunst .config/ -r
 cp $HOME/.config/tmux/tmux.conf .config/tmux/ -r
 cp $HOME/.scripts ./ -r
 cp $HOME/.xinitrc ./ -r
