2960871324fd1b57ef2431276f1d170405d8443c
Mark Christian Lacap <markchristianlacap@gmail.com>
2025-10-20 18:44:13 +0800

    feat: roslyn

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -5,20 +5,20 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+    local mason = require "mason"
+    local mason_lspconfig = require "mason-lspconfig"
+    local mason_tool_installer = require "mason-tool-installer"
 
     -- Core setup
-    mason.setup({
+    mason.setup {
       registries = {
         "github:mason-org/mason-registry",
         "github:crashdummyy/mason-registry",
       },
-    })
+    }
 
     -- Auto-install language servers
-    mason_lspconfig.setup({
+    mason_lspconfig.setup {
       ensure_installed = {
         "eslint",
         "emmet_language_server",
@@ -33,15 +33,15 @@ return {
         "vtsls",
       },
       automatic_installation = true,
-    })
+    }
 
     -- Auto-install formatters and linters
-    mason_tool_installer.setup({
+    mason_tool_installer.setup {
       ensure_installed = {
         "csharpier",
         "cspell",
-        "roslyn-unstable",
+        "roslyn",
       },
-    })
+    }
   end,
 }
