adaf41b7c6dd18315d2b69b14ce3de500e286ec8
Mark Christian Lacap <markchristianlacap@gmail.com>
2026-04-07 15:53:03 +0800

    chore: update

--- .config/nvim/lua/plugins/lsp/blink-cmp.lua
+++ .config/nvim/lua/plugins/lsp/blink-cmp.lua
@@ -19,9 +19,7 @@ return {
 
     appearance = {
       nerd_font_variant = "mono",
-      kind_icon_width = 1,
     },
-
     completion = {
       documentation = {
         auto_show = false,
@@ -60,7 +58,6 @@ return {
         download = true,
       },
     },
-
     signature = {
       enabled = true,
     },
