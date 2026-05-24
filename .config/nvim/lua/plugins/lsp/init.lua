4e4264f9039efb08db98c5b543970c3d64cc4741
Mark Christian Lacap <markchristianlacap@gmail.com>
2026-05-24 20:29:38 +0800

    chore: css ls

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -22,6 +22,7 @@ require("mason-tool-installer").setup({
     "marksman",
     "unocss-language-server",
     "copilot-language-server",
+    "css-lsp",
   },
 })
 
@@ -47,6 +48,7 @@ vim.lsp.enable({
   "marksman",
   "unocss",
   "copilot",
+  "cssls",
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
