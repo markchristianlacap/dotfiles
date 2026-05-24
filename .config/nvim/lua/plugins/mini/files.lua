35da5ab6fe5d1bb8a2f958bd9e9e4ddf651e1e7b
Mark Christian Lacap <markchristianlacap@gmail.com>
2026-05-24 19:01:04 +0800

    chore: mini files new keymap

--- .config/nvim/lua/plugins/mini/files.lua
+++ .config/nvim/lua/plugins/mini/files.lua
@@ -1,8 +1,16 @@
-require("mini.files").setup({
+local MiniFiles = require("mini.files")
+MiniFiles.setup({
   mappings = {
     synchronize = "w",
     go_in_plus = "<CR>",
   },
 })
 
-vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" })
+vim.keymap.set("n", "-", function() MiniFiles.open() end, { desc = "Toggle mini file explorer" })
+
+vim.keymap.set("n", "<leader>e", function()
+      local buf_name = vim.api.nvim_buf_get_name(0)
+      local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+      MiniFiles.open(path)
+      MiniFiles.reveal_cwd()
+    end, { desc = "Open Mini Files" })
