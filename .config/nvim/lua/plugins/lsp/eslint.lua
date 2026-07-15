c77b3b9b967998d1bf2be218c7c610e8615d8242
Mark Christian Lacap <markchristianlacap@gmail.com>
2026-07-15 17:03:13 +0800

    fixed: wrong name

--- .config/nvim/lua/plugins/lsp/eslint.lua
+++ .config/nvim/lua/plugins/lsp/eslint.lua
@@ -1,17 +1,17 @@
 local customizations = {
-  { rule = "style/*", severity = "off", fixable = true },
-  { rule = "format/*", severity = "off", fixable = true },
-  { rule = "*-indent", severity = "off", fixable = true },
+  { rule = "style/*",   severity = "off", fixable = true },
+  { rule = "format/*",  severity = "off", fixable = true },
+  { rule = "*-indent",  severity = "off", fixable = true },
   { rule = "*-spacing", severity = "off", fixable = true },
-  { rule = "*-spaces", severity = "off", fixable = true },
-  { rule = "*-order", severity = "off", fixable = true },
-  { rule = "*-dangle", severity = "off", fixable = true },
+  { rule = "*-spaces",  severity = "off", fixable = true },
+  { rule = "*-order",   severity = "off", fixable = true },
+  { rule = "*-dangle",  severity = "off", fixable = true },
   { rule = "*-newline", severity = "off", fixable = true },
-  { rule = "*quotes", severity = "off", fixable = true },
-  { rule = "*semi", severity = "off", fixable = true },
+  { rule = "*quotes",   severity = "off", fixable = true },
+  { rule = "*semi",     severity = "off", fixable = true },
 }
 
-vim.lsp.config("lua_ls", {
+vim.lsp.config("eslint", {
   filetypes = {
     "javascript",
     "javascriptreact",
