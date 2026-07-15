2b9185e0e9e109d8d68ee979db7b59d466e5e796
Mark Christian Lacap <markchristianlacap@gmail.com>
2026-07-15 16:46:07 +0800

    feat: component highlight

--- .config/nvim/lua/plugins/lsp/typescript.lua
+++ .config/nvim/lua/plugins/lsp/typescript.lua
@@ -1,5 +1,5 @@
 local vue_language_server_path = vim.fn.stdpath("data")
-  .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+    .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
 local vue_plugin = {
   name = "@vue/typescript-plugin",
@@ -10,7 +10,12 @@ local vue_plugin = {
 
 vim.lsp.config("vtsls", {
   on_attach = function(client)
-    client.server_capabilities.documentFormattingProvider = false
+    local existing_capabilities = client.server_capabilities
+    if vim.bo.filetype == "vue" then
+      existing_capabilities.semanticTokensProvider.full = false
+    else
+      existing_capabilities.semanticTokensProvider.full = true
+    end
   end,
   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
   settings = {
@@ -23,9 +28,6 @@ vim.lsp.config("vtsls", {
 })
 
 vim.lsp.config("vue_ls", {
-  on_attach = function(client)
-    client.server_capabilities.documentFormattingProvider = false
-  end,
   settings = {
     vue = {
       suggest = {
